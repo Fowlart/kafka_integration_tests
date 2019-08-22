76ec3ca68c6721c064828ace07129ce3d20068e7
Semikov_Artur <semikov_artur@epam.com>
2019-08-22 19:17:43 +0300

    examples for initialization of primitives added

---
+++ src/main/java/is_double_initializes/ComplexObject.java
@@ -0,0 +1,59 @@
+package is_double_initializes;
+
+import java.util.Optional;
+
+class MoreComplexObject {
+    ComplexObject complexObject;
+
+    public ComplexObject getComplexObject() {
+        return complexObject;
+    }
+
+    public void setComplexObject(ComplexObject complexObject) {
+        this.complexObject = complexObject;
+    }
+}
+
+public class ComplexObject {
+    private java.lang.Double aDouble;
+    private int anInt;
+
+    public Double getaDouble() {
+        return aDouble;
+    }
+
+    public void setaDouble(Double aDouble) {
+        this.aDouble = aDouble;
+    }
+
+    public int getAnInt() {
+        return anInt;
+    }
+
+    public void setAnInt(int anInt) {
+        this.anInt = anInt;
+    }
+
+    public static void main(String[] args) {
+
+        ComplexObject complexObject = new ComplexObject();
+
+        // Primitives are initialized, but nonPrimitives - not
+        System.out.println(complexObject.getaDouble());
+        System.out.println(complexObject.getAnInt());
+
+        complexObject.setaDouble(15d);
+        Optional<Double> doubleOptional = Optional.ofNullable(complexObject.getaDouble());
+        System.out.println(doubleOptional.orElse(0d));
+
+        MoreComplexObject moreComplexObject = new MoreComplexObject();
+
+        // null pointer here
+        //    System.out.println(Optional.ofNullable(moreComplexObject.getComplexObject().getaDouble()).orElse(0d));
+
+        ComplexObject stub = new ComplexObject();
+        stub.setaDouble(111d);
+
+        System.out.println(Optional.ofNullable(moreComplexObject.getComplexObject()).orElse(stub).getaDouble());
+    }
+}
