bc916f85963fae9e8f7cf61886c6e79be61ec042
Semikov_Artur <semikov_artur@epam.com>
2019-10-27 17:50:04 +0200

    27.10 xml study

---
+++ src/main/java/xml/DomParser.java
@@ -0,0 +1,33 @@
+package xml;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
+
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+
+/**
+ * Will read XML from start to end.
+ */
+public class DomParser {
+    public static void main(String[] args) {
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        try {
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+            Document document = documentBuilder.parse("example.xml");
+
+            System.out.println(document.getElementsByTagName("CD").item(0).getFirstChild().getTextContent());
+
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
