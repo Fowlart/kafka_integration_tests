0d465b3f6f66ec45cc5b4445387c8454ea203858
Artur_Semikov <artur_semikov@epam.com>
2019-12-11 16:11:00 +0200

    study 11.12.2019: add Optional example

---
+++ src/main/java/stream_api/Optionals.java
@@ -0,0 +1,11 @@
+package stream_api;
+
+import java.util.Optional;
+
+public class Optionals {
+    public static void main(String[] args) {
+        Integer i = null;
+        Optional<Integer> res =  Optional.ofNullable(i);
+        System.out.println(res.isPresent());
+    }
+}
