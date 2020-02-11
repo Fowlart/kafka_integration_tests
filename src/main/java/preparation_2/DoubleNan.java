996ec950ea6cdf390bb7871a1ce9b8f475b6ef56
Artur_Semikov <artur_semikov@epam.com>
2020-02-11 16:07:00 +0200

    2.11 added NaN example

---
+++ src/main/java/preparation_2/DoubleNan.java
@@ -0,0 +1,9 @@
+package preparation_2;
+
+public class DoubleNan {
+    public static void main(String[] args) {
+        double ZERO = 0;
+        double res = 0.04d/0*ZERO;
+        System.out.println(res);
+    }
+}
